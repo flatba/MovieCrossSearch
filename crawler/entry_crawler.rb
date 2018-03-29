7ccbd353b55328a0371f95b026d221f6a2db8bc8
Yoshiki Hiraba <>
2018-03-29 17:30:54 +0900

    HerokuようにHeadressに変更

--- crawler/entry_crawler.rb
+++ crawler/entry_crawler.rb
@@ -64,18 +64,18 @@ class EntryCrawler
 
   def initialize_driver
     # 通常chrome起動
-    $LOG.debug('chrome起動')
-    Selenium::WebDriver.for :chrome
+    # $LOG.debug('chrome起動')
+    # Selenium::WebDriver.for :chrome
 
     # HeadressChrome起動
-    # $LOG.debug('HeadressChrome起動')
-    # caps = Selenium::WebDriver::Remote::Capabilities.chrome(
-    #   "chromeOptions" => {
-    #     binary: '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary',
-    #     args: ["--headless", "--disable-gpu",  "window-size=1280x800"]
-    #   }
-    # )
-    # Selenium::WebDriver.for :chrome, desired_capabilities: caps
+    $LOG.debug('HeadressChrome起動')
+    caps = Selenium::WebDriver::Remote::Capabilities.chrome(
+      "chromeOptions" => {
+        binary: '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary',
+        args: ["--headless", "--disable-gpu",  "window-size=1280x800"]
+      }
+    )
+    Selenium::WebDriver.for :chrome, desired_capabilities: caps
   end
 
   def initialize_selector
